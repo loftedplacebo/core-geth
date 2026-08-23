27894a4dbef36d9fed549604ac8f87f5f78d7627
Michael Grant <mjgrant@live.co.uk>
2026-08-23 17:42:21 +0100

    Test KawPoW engine header and seal path

--- consensus/kawpowengine/engine_test.go
+++ consensus/kawpowengine/engine_test.go
@@ -7,6 +7,7 @@ import (
 	"math/big"
 	"testing"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/consensus/ethash"
 	"github.com/ethereum/go-ethereum/consensus/kawpow"
 	"github.com/ethereum/go-ethereum/core/types"
@@ -37,6 +38,83 @@ func TestVerifySealRejectsMalformedCandidateInputs(t *testing.T) {
 	}
 	if err := e.VerifySeal(&types.Header{Number: new(big.Int).SetUint64(1 << 31), Difficulty: big.NewInt(2)}); err != kawpow.ErrUnsupportedBlockNumber {
 		t.Fatalf("height-range error = %v, want %v", err, kawpow.ErrUnsupportedBlockNumber)
+	}
+}
+
+func sealedEngineHeader(t testing.TB) *types.Header {
+	t.Helper()
+	header := &types.Header{
+		ParentHash:  common.HexToHash("0x01"),
+		UncleHash:   types.EmptyUncleHash,
+		Coinbase:    common.HexToAddress("0x1234"),
+		Root:        common.HexToHash("0x02"),
+		TxHash:      types.EmptyTxsHash,
+		ReceiptHash: types.EmptyReceiptsHash,
+		Difficulty:  big.NewInt(2),
+		Number:      big.NewInt(42),
+		GasLimit:    30_000_000,
+		Time:        1_700_000_000,
+		Extra:       []byte("aichain-kawpow-engine"),
+	}
+	input, err := kawpow.HeaderToVerificationInput(header)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var headerHash [32]byte
+	copy(headerHash[:], input.SealHash[:])
+	for nonce := uint64(0); ; nonce++ {
+		mix, final, err := kawpow.C1Hash(int(input.Height), headerHash, nonce)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if final[0]&0x80 == 0 {
+			header.Nonce = types.EncodeNonce(nonce)
+			header.MixDigest = common.Hash(mix)
+			return header
+		}
+	}
+}
+
+func TestVerifySealAcceptsValidAndRejectsTampering(t *testing.T) {
+	e := New(ethash.Config{})
+	header := sealedEngineHeader(t)
+	if err := e.VerifySeal(header); err != nil {
+		t.Fatalf("valid seal rejected: %v", err)
+	}
+
+	tamperedMix := types.CopyHeader(header)
+	tamperedMix.MixDigest[0]++
+	if err := e.VerifySeal(tamperedMix); err == nil {
+		t.Fatal("tampered mix accepted")
+	}
+
+	tamperedNonce := types.CopyHeader(header)
+	tamperedNonce.Nonce = types.EncodeNonce(header.Nonce.Uint64() + 1)
+	if err := e.VerifySeal(tamperedNonce); err == nil {
+		t.Fatal("tampered nonce accepted")
+	}
+
+	tamperedDifficulty := types.CopyHeader(header)
+	tamperedDifficulty.Difficulty = big.NewInt(3)
+	if err := e.VerifySeal(tamperedDifficulty); err == nil {
+		t.Fatal("tampered difficulty accepted")
+	}
+}
+
+func TestVerifyHeaderHonoursSealFlag(t *testing.T) {
+	e := New(ethash.Config{PowMode: ethash.ModeFullFake})
+	header := sealedEngineHeader(t)
+	if err := e.VerifyHeader(nil, header, true); err != nil {
+		t.Fatalf("valid sealed header rejected: %v", err)
+	}
+
+	tampered := types.CopyHeader(header)
+	tampered.MixDigest[0]++
+	if err := e.VerifyHeader(nil, tampered, false); err != nil {
+		t.Fatalf("seal-disabled structural check rejected test header: %v", err)
+	}
+	if err := e.VerifyHeader(nil, tampered, true); err == nil {
+		t.Fatal("seal-enabled header check accepted tampered KawPoW mix")
 	}
 }
 
