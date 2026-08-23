4badca775876ce26382960bcb5cc5e3797c2722f
Michael Grant <mjgrant@live.co.uk>
2026-08-23 15:47:38 +0100

    Test KawPoW engine seal rejection matrix

--- consensus/kawpowengine/engine_test.go
+++ consensus/kawpowengine/engine_test.go
@@ -4,8 +4,12 @@
 package kawpowengine
 
 import (
-	"github.com/ethereum/go-ethereum/consensus/ethash"
+	"math/big"
 	"testing"
+
+	"github.com/ethereum/go-ethereum/consensus/ethash"
+	"github.com/ethereum/go-ethereum/consensus/kawpow"
+	"github.com/ethereum/go-ethereum/core/types"
 )
 
 func TestDevelopmentEngineDisablesMining(t *testing.T) {
@@ -15,5 +19,30 @@ func TestDevelopmentEngineDisablesMining(t *testing.T) {
 	}
 	if e.Hashrate() != 0 {
 		t.Fatal("development engine reports a hashrate")
+	}
+}
+
+func TestVerifySealRejectsMalformedCandidateInputs(t *testing.T) {
+	e := New(ethash.Config{})
+
+	if err := e.VerifySeal(nil); err != kawpow.ErrNilHeader {
+		t.Fatalf("nil header error = %v, want %v", err, kawpow.ErrNilHeader)
+	}
+	if err := e.VerifySeal(&types.Header{Number: big.NewInt(1)}); err != kawpow.ErrInvalidDifficulty {
+		t.Fatalf("missing difficulty error = %v, want %v", err, kawpow.ErrInvalidDifficulty)
+	}
+	if err := e.VerifySeal(&types.Header{Number: big.NewInt(1), Difficulty: big.NewInt(1)}); err != kawpow.ErrDifficultyBelowMinimum {
+		t.Fatalf("difficulty-one error = %v, want %v", err, kawpow.ErrDifficultyBelowMinimum)
+	}
+	if err := e.VerifySeal(&types.Header{Number: new(big.Int).SetUint64(1 << 31), Difficulty: big.NewInt(2)}); err != kawpow.ErrUnsupportedBlockNumber {
+		t.Fatalf("height-range error = %v, want %v", err, kawpow.ErrUnsupportedBlockNumber)
+	}
+}
+
+func TestSealHashUsesCandidateMapping(t *testing.T) {
+	e := New(ethash.Config{})
+	h := &types.Header{Number: big.NewInt(1), Difficulty: big.NewInt(2)}
+	if got, want := e.SealHash(h), kawpow.SealHash(h); got != want {
+		t.Fatalf("SealHash = %x, want %x", got, want)
 	}
 }
